cf9e64868707092a0f9113cc565f1ed382ef4269
dhruvinparikh93 <imsilent93@gmail.com>
2020-08-12 19:16:41 -0400

    `WIP` event-selection to wizard

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import { AppBar, Toolbar, Container, Typography } from '@material-ui/core';
-import PrimaryMenuAppBar from '../menu';
-import Dapps from '../../domain/dapps/list-dapps';
+import React from "react";
+import { AppBar, Toolbar, Container, Typography } from "@material-ui/core";
+import PrimaryMenuAppBar from "../menu";
+import Dapps from "../../domain/dapps/list-dapps";
 
-import useStyles from './App.styles';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import useStyles from "./App.styles";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 
-import SelectNotifications from '../../domain/notification/select-notifications'
-import Confirm from '../../domain/notification/confirm';
-import Email from '../../domain/notification/email';
-import ManageSubscriptions from '../../domain/notification/manage-subscriptions';
+import ManageSubscriptions from "../../domain/notification/manage-subscriptions";
+import SubscriptionWizard from "../../domain/subscription-wizard";
 import Snackbar from "../snackbar";
 
 export default function PrimarySearchAppBar() {
@@ -17,33 +15,40 @@ export default function PrimarySearchAppBar() {
 
   return (
     <Router>
+      <div className={classes.grow}>
         <div className={classes.grow}>
-          <div className={classes.grow}>
-            <AppBar position="static">
-              <Toolbar>
-                <Typography className={classes.title} variant="h6" noWrap >
-                  <Link to="/" style={{ color: 'inherit', textDecoration: 'inherit' }} >
-                    N12
+          <AppBar position="static">
+            <Toolbar>
+              <Typography className={classes.title} variant="h6" noWrap>
+                <Link
+                  to="/"
+                  style={{ color: "inherit", textDecoration: "inherit" }}
+                >
+                  N12
                 </Link>
-                </Typography>
+              </Typography>
 
-                <div className={classes.grow} />
-                <PrimaryMenuAppBar />
-              </Toolbar>
-            </AppBar>
-          </div>
-          <Container maxWidth="md">
-            <Switch>
-              <Route path="/select-notifications/:dAppUuid" component={SelectNotifications} />
-              <Route path="/email" component={Email} ></Route>
-              <Route path="/confirm" component={Confirm}></Route>
-              <Route path="/manage-subscriptions/:userUuid" component={ManageSubscriptions}></Route>
-              {/* Keep it last  */}
-              <Route path="/" component={Dapps}></Route>
-            </Switch>
-          </Container>
+              <div className={classes.grow} />
+              <PrimaryMenuAppBar />
+            </Toolbar>
+          </AppBar>
         </div>
-        <Snackbar />
+        <Container maxWidth="md">
+          <Switch>
+            <Route
+              path="/subscription-wizard/:dAppUuid"
+              component={SubscriptionWizard}
+            />
+            <Route
+              path="/manage-subscriptions/:userUuid"
+              component={ManageSubscriptions}
+            ></Route>
+            {/* Keep it last  */}
+            <Route path="/" component={Dapps}></Route>
+          </Switch>
+        </Container>
+      </div>
+      <Snackbar />
     </Router>
   );
 }
--- src/domain/notification/email/index.js
+++ src/domain/notification/email/index.js
@@ -1,65 +1,46 @@
-import React, { useState } from 'react';
-import {Container,CssBaseline,TextField,Button, Typography} from '@material-ui/core';
-import useStyles from './styles';
-import { useDispatch,useSelector } from "react-redux";
-import { updateEmail } from '.././notification.slice';
-import { useHistory } from "react-router-dom";
-import { Notification } from "../notification.slice";
-import CardView from '../../../components/cardView';
+import React /*useState*/ from "react";
+import {
+  Container,
+  CssBaseline,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import useStyles from "./styles";
+import CardView from "../../../components/cardView";
 
-export default function Email() {
+export default function Email(props) {
   const classes = useStyles();
-  const dispatch = useDispatch();
-  const{ email } = useSelector(Notification);
-  const [emailText, setEmail] = useState(email||'');
-  const [isEmailError, setIsEmailError] = useState(false);
-  let history = useHistory();  
-    
-  const handleNext = (e) => {
-    e.preventDefault();
-    if (emailText !== '') {
-      try {
-        dispatch(updateEmail(emailText));  
-        history.push("/confirm")     
-      } catch (err) {
-        setIsEmailError(true);       
-      }
-    } else {
-      if (emailText === '') {
-        setIsEmailError(true);       
-      }
-    }    
-    setEmail('');        
-    }
-    return (
-      <CardView>
-        <Container component="main" maxWidth="xs" className={classes.emailContainer}>
-          <CssBaseline />
-          <form className={classes.form} noValidate>
-            <Typography gutterBottom variant="h5" component="h2">
-              Where would you like to receive the notifications?
-                </Typography>
-            <TextField
-              type="email"
-              error={isEmailError}
-              helperText={isEmailError ? 'Invalid Email.' : ''}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={emailText}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <Button variant="contained" color="primary" onClick={handleNext} >
-              Next
-            </Button>
-          </form>
-        </Container>
-      </CardView>
-    );
+
+  return (
+    <CardView>
+      <Container
+        component="main"
+        maxWidth="xs"
+        className={classes.emailContainer}
+      >
+        <CssBaseline />
+        <form className={classes.form} noValidate>
+          <Typography gutterBottom variant="h5" component="h2">
+            Where would you like to receive the notifications?
+          </Typography>
+          <TextField
+            type="email"
+            error={props.isEmailError}
+            helperText={props.isEmailError ? "Invalid Email." : ""}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            value={props.emailText}
+            onChange={props.handleEmailChange}
+          />
+        </form>
+      </Container>
+    </CardView>
+  );
 }
